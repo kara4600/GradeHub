eb4fd21fcc725b46da524991b202bf8410ea8430
kara4600 <kara178@comcast.net>
2023-04-18 20:49:15 -0700

    Initial Commit

---
+++ GradeHub/GradeHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradeHubApp.swift
+//  GradeHub
+//
+//  Created by Kara Rizzardi on 4/18/23.
+//
+
+import SwiftUI
+
+@main
+struct GradeHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
